a695165cdaf34afab5a83ca2cb98985ec91a572f
Hanif Ardhani <hanif.ardhani98@gmail.com>
2021-03-04 05:25:35 +0000

    platina: Set target bootanimation res

--- conquer_platina.mk
+++ conquer_platina.mk
@@ -21,6 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Inherit some common ConquerOS stuff
 $(call inherit-product, vendor/conquer/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from platina device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
